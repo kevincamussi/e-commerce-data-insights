25442ec514a339062d03504947b385800ca0d6b6
kevincamussi <kevincamussi@gmail.com>
2025-11-01 22:30:25 +0000

    feat: created database file

---
+++ backend/db.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+def get_connection():
+    conn = sqlite3.connect("ecommerce.db")
+    return conn
+
+def create_table():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        category TEXT,
+        price REAL 
+        rating_rate REAL,
+        rating_count INTEGER,
+        profit REAL,
+        profit_margin REAL
+    )
+    """)
+    conn.commit()
+    conn.close()
+    print("Table created (or already exists).")
